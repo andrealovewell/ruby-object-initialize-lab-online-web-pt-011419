2550d335a1dfbbd800e752ec7bd33f1bc4fcb578
Andrea Lovewell <andy.lovewell@gmail.com>
2019-02-01 05:58:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,15 +12,15 @@ class Dog
   def name=(name)
     @name = name
   end
-  
+
   def breed=(breed)
     @breed = breed
   end
-  
+
   def name
     @name
   end
-  
+
   def breed
     @breed
   end
