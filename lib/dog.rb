0597bc76c54f7e61a7dd5d81a90c848ff9c3d3aa
Andrea Lovewell <andy.lovewell@gmail.com>
2019-02-01 05:55:29 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def initialize(breed)
+    if(breed == nil)
+      @breed = "Mutt"
+    else
+      @breed = breed
+    end
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
